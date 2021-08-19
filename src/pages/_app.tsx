2caa63590e29e4387508dbc167c8412b1b0ed3af
Andrei <vitobravis@gmail.com>
2021-08-19 18:29:01 +0700

    add recoil root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,25 @@ import { SwitchTransition, Transition } from 'react-transition-group';
 import GlobalStyle from "../style/globalStyles";
 import '../style/fonts.css';
 import ScrollBarProvider from "../context/ScrollBarContext";
+import {RecoilRoot} from "recoil";
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
     const { asPath } = router;
 
     return (
-        <ScrollBarProvider>
-            <GlobalStyle />
-            <SwitchTransition>
-                <Transition
-                    key={asPath}
-                    timeout={600}
-                >
-                    <Component {...pageProps} />
-                </Transition>
-            </SwitchTransition>
-        </ScrollBarProvider>
+        <RecoilRoot>
+            <ScrollBarProvider>
+                <GlobalStyle />
+                <SwitchTransition>
+                    <Transition
+                        key={asPath}
+                        timeout={600}
+                    >
+                        <Component {...pageProps} />
+                    </Transition>
+                </SwitchTransition>
+            </ScrollBarProvider>
+        </RecoilRoot>
     )
 }
 
